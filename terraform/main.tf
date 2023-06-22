103b77bc5603c916f69ca81c47f566fc1b09cf38
kashafarshad <kashafarshad@hotmail.co.uk>
2023-06-22 21:19:58 +0200

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "home_api" {
   function_name = var.lambda_function_name
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "python/app.lamda_homepage"
+  handler       = "python/app.lambda_handler"
   filename      = "deployment.zip"
   depends_on = [
     aws_iam_role_policy_attachment.lambda_logs,
